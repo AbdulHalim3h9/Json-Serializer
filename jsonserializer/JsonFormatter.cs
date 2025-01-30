b551cad410ec8cf856ca2f020a931fd8e29a8f5a
AbdulHalim3h9 <abdulhalimkhan271@gmail.com>
2025-01-30 19:14:38 +0600

    fixed typos

---
+++ jsonserializer/JsonFormatter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_1
+{
+    public class JsonFormatter
+    {
+        public static string Convert(object input)
+        {
+            int iindex = 1;
+            StringBuilder JsonTextSB = new StringBuilder();
+            
+            static string ConvertToJson(object inputText, StringBuilder SB, int index)
+            {
+                Type t = inputText.GetType();
+                PropertyInfo[] properties = t.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+
+                //To add comma after '}'
+                for (int i = SB.Length - 1; i >= 0; i--)
+                {
+                    if (SB[i].ToString().Equals("]"))
+                    {
+                        SB[i + 1] = ',';
+                        break;
+                    }
+                    if (SB[i].ToString().Equals("}"))
+                    {
+                        SB[i + 1] = ',';
+                        break;
+                    }
+                }
+
+                SB.AppendLine("{");
+                foreach (var item in properties)
+                {
+                    for (int i = 0; i < index; ++i)
+                    {
+                        //adding some space to indent the document
+                        SB.Append("   ");
+                    }
+                    SB.Append($"\"{item.Name}\":");
+                    if (item.GetValue(inputText).ToString().StartsWith($"{t.Namespace}"))
+                    {
+
+                        ConvertToJson(item.GetValue(inputText), SB, ++index);
+                        --index;
+                    }
+                    else if (item.GetValue(inputText).ToString().Contains("System.Collections") && item.GetValue(inputText).ToString().Contains($"{t.Namespace}"))
+                    {
+                        // To Add a comma
+                        for (int i = SB.Length - 1; i >= 0; i--)
+                        {
+                            if (SB[i].ToString().Equals("}"))
+                            {
+                                break;
+                            }
+                            if (SB[i].ToString().Equals("]"))
+                            {
+                                SB[i + 1] = ',';
+                                break;
+                            }
+                        }
+                        SB.AppendLine("[");
+                        var lst = item.GetValue(inputText);
+                        foreach (var lt in (dynamic)lst)
+                        {
+                            for (int i = 0; i < index; ++i)
+                            {
+                                //indenting
+                                SB.Append("   ");
+                            }
+                            ConvertToJson(lt, SB, ++index);
+                            --index;
+                        }
+                        for (int i = 0; i < index; ++i)
+                        {
+                            //Indenting
+                            SB.Append("   ");
+                        }
+                        SB.AppendLine("]");
+                    }
+                    else
+                    {
+                        SB.Append($"\"{item.GetValue(inputText)}\",\n");
+                    }
+
+                    if (item == properties.Last())
+                    {
+                        // Removing The Comma for the last pair
+                        SB.Remove(SB.Length - 2, 2);
+                        SB.Append("\n");
+                    }
+                }
+                for (int i = 1; i < index; i++)
+                {
+                    //indenting
+                    SB.Append("   ");
+                }
+                SB.AppendLine("}");
+                string Jsontext = SB.ToString();
+                return Jsontext;
+            }
+            return ConvertToJson(input, JsonTextSB, iindex);
+        }
+    }
+
+}
---
+++ jsonserializer/Program.cs
@@ -0,0 +1,78 @@
+﻿using assignment_1;
+using System.Reflection;
+using System.Text;
+
+Course course = new Course();
+course.Title = "Asp.net";
+course.Fees = 30000;
+course.Gender = new Gender
+{
+    IsMale = true
+};
+course.Teacher = new Instructor()
+{
+    Name = "Jalaluddin",
+    Email = "jalal@gmail.com",
+    PresentAddress = new Address()
+    {
+        Street = "Senpara1",
+        City = "Dhaka",
+        Country = "Bangladesh"
+    },
+    PermanentAddress = new Address()
+    {
+        Street = "Senpara1",
+        City = "Dhaka",
+        Country = "Bangladesh"
+    },
+    PhoneNumbers = new List<Phone>
+    {
+        new Phone()
+        {
+            Number = "12345678910",
+            Extension = "141",
+            CountryCode = "+880"
+        },
+        new Phone()
+        {
+            Number = "42111145678910",
+            Extension = "151",
+            CountryCode = "+880"
+        },
+        new Phone()
+        {
+            Number = "151545678910",
+            Extension = "171",
+            CountryCode = "+880"
+        }
+    }
+    
+};
+course.Topics = new List<Topic>
+{
+    new Topic()
+    {
+        Title = "Asp.net",
+        Description = "Asp.net core MVC",
+        Sessions = new List<Session>
+        {
+            new Session()
+            {
+                DurationInHour = 400,
+                LearningObjective = "To Help Undrstand ASP.net core MVC"
+            }
+        }
+    }
+};
+course.Tests = new List<AdmissionTest>
+{
+    new AdmissionTest()
+    {
+        StartDateTime = DateTime.Now,
+        EndDateTime = DateTime.Now,
+        TestFees = 100
+    }
+};
+
+string txt = JsonFormatter.Convert(course);
+Console.WriteLine(txt);
